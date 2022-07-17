69a46fd634b5bbbacfebdda51a393a0f2491b1f1
Anton Volyk <anton_volyk@epam.com>
2022-07-17 20:57:31 +0300

    add jest unit test

--- product-service/src/functions/get-products-list/handler.ts
+++ product-service/src/functions/get-products-list/handler.ts
@@ -1,9 +1,9 @@
-import { formatJSONResponse } from '@libs/api-gateway';
 import products from '../../mocks/products.json';
+import { formatJSONResponse } from '@libs/api-gateway';
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { middyfy } from "@libs/lambda";
-import {SUCCESS_STATUS_CODE} from "../../constants/http-response";
+import { SUCCESS_STATUS_CODE } from "../../constants/http-response";
 
-const getProductsList : APIGatewayProxyHandler = async () => formatJSONResponse(products, SUCCESS_STATUS_CODE);
+export const getProductsList : APIGatewayProxyHandler = async () => formatJSONResponse(products, SUCCESS_STATUS_CODE);
 
 export const main = middyfy(getProductsList);
